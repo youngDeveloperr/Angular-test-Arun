0074fc419bb9a8b6ebf5f40a86c4d201d2119319
Arun <arunraju9837@gmail.com>
2024-01-26 14:18:50 +0530

    FIX :: Add  selctors to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StoryFormComponent } from './story-form/story-form.component';
+import { StoryListingComponent } from './story-listing/story-listing.component';
+import { SprintCalculatorComponent } from './sprint-calculator/sprint-calculator.component';
+import { IStoryData } from './story.types';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    StoryFormComponent,
+    StoryListingComponent,
+    SprintCalculatorComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'sprint-planner';
